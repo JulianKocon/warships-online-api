f5783dc6d99bc73911bb55ac7cf7d8fdd373ad55
Julian Kocon <77498123+JulianKocon@users.noreply.github.com>
2023-05-02 19:59:10 +0200

    Update boostStats.sh

--- boostStats.sh
+++ boostStats.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-i = 0
+i=0
 while true;
 do
     name=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 6 ; echo '')
     i=$((i+1))
-    echo "Run $i"
-    go run main.go --nick "$name" --abandon  &
+    echo "Win streak: $i"
+    go run main.go --nick "$name" --abandon &> /dev/null &
     sleep 0.5
     go run main.go --nick JulianKocon --target_nick "$name" &> /dev/null
     wait
